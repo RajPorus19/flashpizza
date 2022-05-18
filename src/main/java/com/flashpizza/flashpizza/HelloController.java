61bab39aed383425564d6d5f25ac785e19dd4df0
RajPorus19 <55143326+RajPorus19@users.noreply.github.com>
2022-05-18 11:13:59 +0200

    initial commit

---
+++ src/main/java/com/flashpizza/flashpizza/HelloController.java
@@ -0,0 +1,27 @@
+package com.flashpizza.flashpizza;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+
+    @GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot! ça marche !";
+	}
+
+}
---
+++ src/test/java/com/flashpizza/flashpizza/FlashpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashpizza.flashpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
