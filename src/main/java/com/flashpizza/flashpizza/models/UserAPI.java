253aa7c8a805aac70f6be3b887120eb78910b597
RajPorus19 <55143326+RajPorus19@users.noreply.github.com>
2022-06-20 20:17:09 +0200

    balancestr

--- src/main/java/com/flashpizza/flashpizza/models/UserAPI.java
+++ src/main/java/com/flashpizza/flashpizza/models/UserAPI.java
@@ -1,5 +1,6 @@
 package com.flashpizza.flashpizza.models;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
@@ -29,9 +30,10 @@ public class UserAPI {
     			String password  = res.getString("password");
     			String email  = res.getString("email");
     			String phone_number  = res.getString("phone_number");
-				String balance  = res.getString("balance");
+				Double balance= res.getDouble("balance");
+				String balanceStr = Double.toString(balance);
     		
-				User user = new User(id,firstname,password,email,phone_number, balance);
+				User user = new User(id,firstname,password,email,phone_number, balanceStr);
     			users_list.add(user);
     		}
     	}
@@ -71,56 +73,6 @@ public class UserAPI {
 		sql += "password = '" + user.getPassword() + "'";
 		sql += "WHERE id=" + user.getId();
 		db.update_db(sql);
-	}
-	public String unwrapped_get_current_order_id(String userId) throws SQLException{
-		String basketId = "1";
-		String sql = "select id from order WHERE user_id="+userId+" and state_id="+basketId;
-		System.out.println(sql);
-		ResultSet res = db.query_db(sql);
-		if(res!=null){
-        	while(res.next()) {
-				String id = Integer.toString(res.getInt("id"));
-				return id;
-			}
-		}
-		return null;
-	}
-	public String get_current_order_id(String userId) throws SQLException{
-		String res = unwrapped_get_current_order_id(userId);
-		if(res == null){
-			Database ndb = new Database();
-			String sql = "insert into order (user_id) VALUES ("+userId+")";
-			System.out.println(sql);
-		}
-		res = unwrapped_get_current_order_id(userId);
-		return res;
-	};
-	public void addOrderline(String userId,String pizza_id, String size_id,String quantity) throws SQLException{
-		String  orderId = get_current_order_id(userId);
-		String sql = "insert into order_line (order_id,pizza_id,size_id,quantity) VALUES ("
-		+ orderId + "," + pizza_id + "," + quantity +")";
-		System.out.print(sql);
-		db.update_db(sql);
-	}
-	public ArrayList<OrderLine> getOrderLines(String userId) throws SQLException{
-		String orderId = get_current_order_id(userId);
-		ResultSet res = db.query_db("select * from order_line where order_id="+orderId);
-		ArrayList<OrderLine> list_order_lines = new ArrayList<OrderLine>();
-    	if(res != null) {
-        	while(res.next()) {
-				String id = Integer.toString(res.getInt("id"));
-				String order_id = Integer.toString(res.getInt("order_id"));
-				String pizza_id= Integer.toString(res.getInt("pizza_id"));
-				String size_id = Integer.toString(res.getInt("size_id"));
-				String quantity= Integer.toString(res.getInt("quantity"));
-				String price= Double.toString(res.getDouble("price"));
-    		
-				OrderLine orderLine = new OrderLine(id,order_id,pizza_id,size_id,quantity,price);
-    			list_order_lines.add(orderLine);
-    		}
-    	}
-		return list_order_lines;
-
 	}
 
 	public void addBalance(String userId, String amount) {
